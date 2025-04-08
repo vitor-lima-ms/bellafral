99a18a72141770ff0fa9d1b6a22d509819d662fd
Vitor Lima <viitorlima2000@gmail.com>
2025-04-08 13:35:57 -0300

    Deleting files

--- stock/my_forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from stock.models import Stock
-
-class StockForm(forms.ModelForm):
-    class Meta:
-        model = Stock
-        fields = '__all__'
--- stock/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from stock import views
-
-app_name = 'stock'
-
-urlpatterns = [
-    path('stock_form', views.stock_form, name='stock_form'),
-]
