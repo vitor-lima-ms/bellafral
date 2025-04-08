5248fd44ff020d843b1adda73d6e4e9b20121630
Vitor Lima <viitorlima2000@gmail.com>
2025-04-08 15:14:41 -0300

    Update models.py

--- costs/models.py
+++ costs/models.py
@@ -3,15 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Costs(models.Model):
-    nome = models.CharField(max_length=100, default='')
-    celulose_virgem_price = models.DecimalField(max_digits=10, decimal_places=4, default=0)
-    gel_price = models.DecimalField(max_digits=10, decimal_places=4, default=0)
-    tnt_filtrante_780_price = models.DecimalField(max_digits=10, decimal_places=4, default=0)
-    fita_adesiva_tape_price = models.DecimalField(max_digits=10, decimal_places=4, default=0)
-    elastico_elastano_lycra_price = models.DecimalField(max_digits=10, decimal_places=4, default=0)
-    barreira_price = models.DecimalField(max_digits=10, decimal_places=4, default=0)
-    polietileno_filme_780_price = models.DecimalField(max_digits=10, decimal_places=4, default=0)
-    hot_melt_const_price = models.DecimalField(max_digits=10, decimal_places=4, default=0)
+    identificador = models.CharField(max_length=100, default='')
+    celulose_virgem_price = models.DecimalField(verbose_name='Celulose virgem (R$)', max_digits=10, decimal_places=4, default=0)
+    gel_price = models.DecimalField(verbose_name='Gel (R$)', max_digits=10, decimal_places=4, default=0)
+    tnt_filtrante_780_price = models.DecimalField(verbose_name='TNT filtrante 780 (R$)', max_digits=10, decimal_places=4, default=0)
+    fita_adesiva_tape_price = models.DecimalField(verbose_name='Fita adesiva tape (R$)', max_digits=10, decimal_places=4, default=0)
+    elastico_elastano_lycra_price = models.DecimalField(verbose_name='Elástico elástano lycra (R$)', max_digits=10, decimal_places=4, default=0)
+    barreira_price = models.DecimalField(verbose_name='Barreira (R$)', max_digits=10, decimal_places=4, default=0)
+    polietileno_filme_780_price = models.DecimalField(verbose_name='Polietileno filme 780 (R$)', max_digits=10, decimal_places=4, default=0)
+    hot_melt_const_price = models.DecimalField(verbose_name='Hot melt const (R$)', max_digits=10, decimal_places=4, default=0)
 
     def __str__(self):
-        return self.nome
+        return self.identificador
