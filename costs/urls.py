c21db0aeb7fa9b7d486520a1857b96108ceac427
Vitor Lima <viitorlima2000@gmail.com>
2025-04-08 15:14:36 -0300

    Update urls.py

--- costs/urls.py
+++ costs/urls.py
@@ -5,4 +5,10 @@ app_name = 'costs'
 
 urlpatterns = [
     path('costs_form', views.costs_form, name='costs_form'),
+
+    path('costs_list', views.costs_list, name='costs_list'),
+
+    path('costs_details/<int:id>', views.costs_details, name='costs_details'),
+
+    path('costs_delete/<int:id>', views.costs_delete, name='costs_delete'),
 ]
