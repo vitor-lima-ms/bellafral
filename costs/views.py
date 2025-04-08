149bfe35bfd42743df4a68c8960c907e4b036c8a
Vitor Lima <viitorlima2000@gmail.com>
2025-04-08 15:14:35 -0300

    Update views.py

--- costs/views.py
+++ costs/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from costs.my_forms import CostsForm
+from costs.models import Costs
+
 # Create your views here.
 
 def costs_form(request):
@@ -13,3 +15,16 @@ def costs_form(request):
     else:
         form = CostsForm()
     return render(request, 'costs_form.html', {'form': form})
+
+def costs_list(request):
+    costs = Costs.objects.all()
+    return render(request, 'costs_list.html', {'costs': costs})
+
+def costs_details(request, id):
+    cost = get_object_or_404(Costs, id=id)
+    return render(request, 'costs_details.html', {'cost': cost})
+
+def costs_delete(request, id):
+    cost = get_object_or_404(Costs, id=id)
+    cost.delete()
+    return redirect('costs:costs_list')
