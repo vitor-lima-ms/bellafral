e6f4b88b7ab4ba229dcacac328b2f7ad8facdfa5
Vitor Lima <viitorlima2000@gmail.com>
2025-04-11 23:00:37 -0300

    Update settings.py

--- base_dir/settings.py
+++ base_dir/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+2u=r31b36y5zsfga_w0rrxklvlu%d(h4@r(@6*#tb%u+ab@a+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'bellafralcustos.com',
